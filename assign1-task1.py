cffb68446f7326e02cb38f449c94c1c34d1ace75
Beerajm <beerajm@gmail.com>
2026-01-31 13:13:22 +0530

    inital commit

---
+++ assign1-task1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number ", ))
+num2 = int(input("Enter the second number ", ))
+
+Add = num1+num2
+Sub = num1-num2
+Mult = num1*num2
+Div = num1/num2
+
+print("Addition = ", Add)
+print("Substraction = ", Sub)
+print("Multiplication = ", Mult)
+print("Division = ", round(Div, 2))
---
+++ assign1-task2.py
@@ -0,0 +1,6 @@
+First_name = (input("Enter your first name: ", ))
+Last_name = (input("Enter your last name: ", ))
+
+Name = First_name + " " + Last_name
+print("Hello,", Name,'!! ' "Happy New Year 2026 !!")
+
